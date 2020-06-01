5fdc63616ba25a75df2ce113d7045194489c7604
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2020-06-01 19:11:22 +0200

    Extending ThreadPool with queue
      - Adding basic thread queue and interface to enqueue the jobs on
        threads

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -2,10 +2,15 @@
 #include <vector>
 #include <thread>
 #include <condition_variable>
+#include <queue>
+#include <memory>
 
 class ThreadPool
 {
 public:
+
+  using Task = std::function<void()>;
+
   explicit ThreadPool(std::size_t numThreads)
   {
     Start(numThreads);
@@ -14,6 +19,14 @@ public:
   ~ThreadPool()
   {
     Stop();
+  }
+
+  void Enqueue(Task task)
+  {
+    {
+      std::unique_lock<std::mutex> lock{mEventMutex};
+      mTasks.emplace(std::move(task));
+    }
   }
 
 private:
@@ -28,6 +41,8 @@ private:
   // std::atomic<bool>?
   bool mStopping = false;
 
+  std::queue<Task> mTasks;
+
   void Start(std::size_t numThreads)
   {
     // range-based for loop?
@@ -37,12 +52,28 @@ private:
                              {
         while (true)
         {
-          std::unique_lock<std::mutex> lock{mEventMutex};
-          mEvent.wait(lock, [=]{return mStopping;});
-          if (mStopping)
+          Task task;
+
+          // Additional scope for the mutex to be locked only during critical
+          // section. It should not be locked during the task execution.
           {
-            break;
+            std::unique_lock<std::mutex> lock{mEventMutex};
+            mEvent.wait(lock, [=]{return mStopping || !mTasks.empty();});
+            if (mStopping)
+            {
+              break;
+            }
+
+            task = std::move(mTasks.front());
+
+            // Better function for popping the task?
+            // Here it should be just the removal from the queue?
+            mTasks.pop();
+
+            // The lock will be freed here, after leaving the scope
           }
+
+          task();
         }
                              });
     }
